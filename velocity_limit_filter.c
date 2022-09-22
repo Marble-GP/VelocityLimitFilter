bd92f25df35b4eff93b456b04629337da1d72fba
Marble-GP <74084344+Marble-GP@users.noreply.github.com>
2022-09-22 16:40:17 +0900

    Add files via upload

--- velocity_limit_filter.c
+++ velocity_limit_filter.c
@@ -2,9 +2,49 @@
 
 void VLF_Init(velocity_limit_filter_t* handler, float max_velocity, float FB_Gain, float LPF_CutOffFreq, float ControlFreq)
 {
+
+    //auto LPF gain
+    if(LPF_CutOffFreq < 0.0f)
+    {
+        float k = ControlFreq/max_velocity;
+        if(k > 10.0f)
+        {
+            LPF_CutOffFreq = 10.0f * max_velocity;
+            if(FB_Gain < 0.0f)
+            {
+                FB_Gain = 15.4f*max_velocity;
+            }
+        }
+        else if(k > 5.0f)
+        {
+            LPF_CutOffFreq = 5.0f*max_velocity;
+            if(FB_Gain < 0.0f)
+            {
+                FB_Gain = 7.82f*max_velocity;
+            }
+        }
+        else if(k > 2.0f)
+        {
+            LPF_CutOffFreq = 2.0f*max_velocity;
+            if(FB_Gain < 0.0f)
+            {
+                FB_Gain = 3.18f*max_velocity;
+            }           
+        }
+        else
+        {
+            LPF_CutOffFreq = max_velocity;
+            if(FB_Gain < 0.0f)
+            {
+                FB_Gain = 1.65f*max_velocity;
+            }
+        }
+
+    }
+
     handler->max_velocity = max_velocity;
-    handler->FB_Gain = FB_Gain > 0.0f ? FB_Gain : 1.653f*max_velocity;
-    handler->LPF_Gain = LPF_CutOffFreq > 0.0f ? (2.0f*VLF_FLOAT32_PI*LPF_CutOffFreq/ControlFreq)/(1.0f + (2.0f*VLF_FLOAT32_PI*LPF_CutOffFreq/ControlFreq)) : (2.0f*VLF_FLOAT32_PI*max_velocity/ControlFreq)/(1.0f + (2.0f*VLF_FLOAT32_PI*max_velocity/ControlFreq));
+    handler->FB_Gain = FB_Gain;
+    handler->LPF_Gain = (2.0f*VLF_FLOAT32_PI*LPF_CutOffFreq/ControlFreq)/(1.0f + (2.0f*VLF_FLOAT32_PI*LPF_CutOffFreq/ControlFreq));
     handler->Ts = 1.0f/ControlFreq;
     handler->output = 0.0f;
     handler->input_lpf_pre = 0.0f;
--- velocity_limit_filter.h
+++ velocity_limit_filter.h
@@ -4,8 +4,8 @@
  * @brief This is a Library of Velocity Limit Filter with Feedback (Proposed by H.Okajima,et al., Kumamoto Univ.)
  * @ref https://www.jstage.jst.go.jp/article/sicetr/54/1/54_146/_pdf/-char/ja
  *      https://fragrant-rainforest-e05.notion.site/ecb6808ef9f348749128c61bf45e40c4
- * @version 0.2
- * @date 2022-09-20
+ * @version 0.2.2
+ * @date 2022-09-22
  * 
  * @copyright Copyright (c) 2022
  * 
@@ -14,9 +14,13 @@
 #ifndef VELOCITY_LIMIT_FILTER_H
 #define VELOCITY_LIMIT_FILTER_H
 
+#include <math.h>
+
 #define VLF_FLOAT32_PI (3.1415927f)
 
 #define VLF_limitter(x, limit_val) ((x) > (limit_val) ? (limit_val) : ((x) < -(limit_val) ? -(limit_val) : (x)))
+
+#define VLF_AUTO (-1.0f)
 
 typedef struct __velocity_limit_filter
 {
